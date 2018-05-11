8adb110888ece6c292bd59eb4c4f642a1998c6ad
Medomedoch <anicka.serych@gmail.com>
2018-05-11 19:19:01 +0200

    hezke tvary

--- ctverec.py
+++ ctverec.py
@@ -1,42 +1,49 @@
-def ctverec(velikost, x):
-	radka = x + " " * (velikost - 2) + x
-	print(x * velikost)
-	print((radka + "\n") * (velikost - 3) + radka ) 
-	print(x * velikost)
+def ctverec(velikost, d="-", e="|", f="+"):
+	
+	vnitrek = e + " " * (velikost - 2) + e
+	kraj = f + d *  (velikost - 2) + f
+
+	print(kraj)
+	for i in range(velikost - 2):
+		print(vnitrek)
+	print(kraj)
 
 def lichy(n):
 	return velikost % 2 == 1
 
-def trojuhelnik(velikost, x):
+def trojuhelnik(velikost, x='_', a="\\", b="/", c="V"):
+	
 	print(x * velikost)
 	mezera = velikost - 4
 	i = 1
 	while mezera >= 0:
-		print((" " * i) + x + (" " * mezera) + x)
+		print((" " * i) + a + (" " * mezera) + b)
 		i = 1 + i 
 		mezera = velikost - (2 + 2*i)
 
 	if lichy(velikost):
-		print((" " * i) + x)
+		print((" " * i) + c)
 
 
 
+def main ():
+	povolene = ["a", "b"]
+	tvar = input("Mam ti nakreslit ctverec (a) nebo trojuhelnik (b)?\n")
+	if tvar not in povolene:
+		print ("ty jsi ale voresprut!")
+		return
+	velikost = input("A jak velký?\n")
+	velikost = int(velikost)
+		
 
+	if tvar == "a":
+		print("ctverec")
+		ctverec(velikost)
+	elif tvar == "b":
+		print("trojuhelnik")
+		trojuhelnik(velikost)
 
-tvar = input("Mam ti nakreslit ctverec (a) nebo trojuhelnik (b)?\n")
-velikost = input("A jak velký?\n")
-velikost = int(velikost)
-x = "O"
+	print("")
 
-if tvar == "a":
-	print("ctverec")
-	ctverec(velikost, x)
-elif tvar == "b":
-	print("trojuhelnik")
-	trojuhelnik(velikost, x)
-else:
-	print ("ty jsi ale voresprut!")
-
-print("")
-
+main()
 
